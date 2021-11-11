8bcc87bd30461e05bcb4e9088ebcb03affb2c405
ajay <ajay.thakur@anthive.tech>
2021-11-11 10:40:31 +0530

    post

--- components/SignedMainContent.js
+++ components/SignedMainContent.js
@@ -1,132 +1,132 @@
 
 const SignedMainContent = () => (
 
-<div className="main_middle"> 
-    
-  <div className="write_post">
-    <div className="team_slider">
-      <ul className="user_slider">
-        <li><img src="/assets/media/dash/user.jpg" alt=""/></li>
-        <li><img src="/assets/media/dash/user.jpg" alt=""/></li>
-        <li><img src="/assets/media/dash/user.jpg" alt=""/></li>
-        <li><img src="/assets/media/dash/user.jpg" alt=""/></li>
-      </ul>
-    </div>
-    <textarea placeholder="Write a post"></textarea>
-    <div className="right_links"> 
-        <div className="post_img"><input type="file" id="files" name="files[]" multiple/></div>
-        <a href="#"><img src="/assets/media/dash/cal-icon.png" alt=""/></a>
-         <a href="#"><img src="/assets/media/dash/game-icon.png" alt=""/></a> 
-         <a href="#"><img src="/assets/media/dash/live-icon.png" alt=""/></a> 
-         <a href="#"><img src="/assets/media/dash/twitter-icon.png" alt=""/></a> </div>
-  </div>
-  
-  
-  <ul className="three_nav">
-    <li><a href="#">For You</a></li>
-    <li><a href="#">Following</a></li>
-    <li><a href="#">Discover</a></li>
-  </ul>
-  
-  
-  <div className="for-you"> 
-    
-    
-    <div className="post">
-      <div className="heads">
-        <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
-        <h4>TheMadTitan</h4>
+  <div className="main_middle"> 
+      
+    <div className="write_post">
+      <div className="team_slider">
+        <ul className="user_slider">
+          <li><img src="/assets/media/dash/user.jpg" alt=""/></li>
+          <li><img src="/assets/media/dash/user.jpg" alt=""/></li>
+          <li><img src="/assets/media/dash/user.jpg" alt=""/></li>
+          <li><img src="/assets/media/dash/user.jpg" alt=""/></li>
+        </ul>
       </div>
-      <div className="left_details"> <a href="#"> <i className="fa fa-heart" aria-hidden="true"></i> <span>1.7k</span> </a> <a href="#"> <i className="fa fa-eye" aria-hidden="true"></i> <span>239k</span> </a> <a href="#"> <i className="fa fa-commenting" aria-hidden="true"></i> <span>232k</span> </a> </div>
-      <div className="right_details">
-        <div className="post_data"></div>
-        <div className="users_share_box">
-          <div className="more_user"> <a href="#"><img src="/assets/media/dash/1.jpg" alt="user"/><span className="online"></span></a> <a href="#"><img src="/assets/media/dash/2.jpg" alt="user"/><span className="online"></span></a> <a href="#"><img src="/assets/media/dash/3.jpg" alt="user"/><span className="offiline"></span></a> <a href="#" className="more">+3</a> <span className="others">Ashwin, George and 5 others have liked your post.</span> </div>
-          <div className="shere"> <a href="#"> <i className="fa fa-heart" aria-hidden="true"></i> <span>Like</span> </a> <a href="#"> <i className="fa fa-share-alt" aria-hidden="true"></i> <span>Share</span> </a>
-            <div className="three_dots"><a href="#"> <i className="fa fa-ellipsis-v" aria-hidden="true"></i></a>
-              <div className="three_dots_dropdown">
-                <ul>
-                  <li><a href="#">Edit</a></li>
-                  <li><a href="#">Share to</a></li>
-                  <li><a href="#">Copy Link</a></li>
-                  <li><a href="#">Delet</a></li>
-                </ul>
+      <textarea placeholder="Write a post"></textarea>
+      <div className="right_links"> 
+          <div className="post_img"><input type="file" id="files" name="files[]" multiple/></div>
+          <a href="#"><img src="/assets/media/dash/cal-icon.png" alt=""/></a>
+           <a href="#"><img src="/assets/media/dash/game-icon.png" alt=""/></a> 
+           <a href="#"><img src="/assets/media/dash/live-icon.png" alt=""/></a> 
+           <a href="#"><img src="/assets/media/dash/twitter-icon.png" alt=""/></a> </div>
+    </div>
+    
+    
+    <ul className="three_nav">
+      <li><a href="#">For You</a></li>
+      <li><a href="#">Following</a></li>
+      <li><a href="#">Discover</a></li>
+    </ul>
+    
+    
+    <div className="for-you"> 
+      
+      
+      <div className="post">
+        <div className="heads">
+          <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
+          <h4>TheMadTitan</h4>
+        </div>
+        <div className="left_details"> <a href="#"> <i className="fa fa-heart" aria-hidden="true"></i> <span>1.7k</span> </a> <a href="#"> <i className="fa fa-eye" aria-hidden="true"></i> <span>239k</span> </a> <a href="#"> <i className="fa fa-commenting" aria-hidden="true"></i> <span>232k</span> </a> </div>
+        <div className="right_details">
+          <div className="post_data"></div>
+          <div className="users_share_box">
+            <div className="more_user"> <a href="#"><img src="/assets/media/dash/1.jpg" alt="user"/><span className="online"></span></a> <a href="#"><img src="/assets/media/dash/2.jpg" alt="user"/><span className="online"></span></a> <a href="#"><img src="/assets/media/dash/3.jpg" alt="user"/><span className="offiline"></span></a> <a href="#" className="more">+3</a> <span className="others">Ashwin, George and 5 others have liked your post.</span> </div>
+            <div className="shere"> <a href="#"> <i className="fa fa-heart" aria-hidden="true"></i> <span>Like</span> </a> <a href="#"> <i className="fa fa-share-alt" aria-hidden="true"></i> <span>Share</span> </a>
+              <div className="three_dots"><a href="#"> <i className="fa fa-ellipsis-v" aria-hidden="true"></i></a>
+                <div className="three_dots_dropdown">
+                  <ul>
+                    <li><a href="#">Edit</a></li>
+                    <li><a href="#">Share to</a></li>
+                    <li><a href="#">Copy Link</a></li>
+                    <li><a href="#">Delet</a></li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
+          
+          
+          <div className="add_comment_box">
+            <div className="add_comments">
+              <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
+              <textarea placeholder="Add a comment"></textarea>
+              <a href="#" className="gif">GIF</a> <a href="#" className="smile"><img src="/assets/media/dash/smile.png" alt=""/></a> </div>
+            <button><img src="/assets/media/dash/send.png" alt=""/></button>
+          </div>
+          
+          
+          
+          <div className="post_comments">
+            <div className="pop_comment">Popular Comments</div>
+            <div className="comments_point">
+              <div className="fire"><img src="/assets/media/dash/fire.png" alt=""/> <span>45</span></div>
+              <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
+              <h3>TheMadTitan</h3>
+              <a href="#" className="create">Creator</a> <span className="days">2 days ago</span> <a href="#" className="pinned">Pinned by Creator</a> </div>
+            <p>Thank you everyone for all of your support.</p>
+            <div className="loadmore"><a href="#">Load comments <i className="fa fa-angle-down"
+                                      aria-hidden="true"></i></a></div>
+          </div>
+          
+          
         </div>
-        
-        
-        <div className="add_comment_box">
-          <div className="add_comments">
-            <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
-            <textarea placeholder="Add a comment"></textarea>
-            <a href="#" className="gif">GIF</a> <a href="#" className="smile"><img src="/assets/media/dash/smile.png" alt=""/></a> </div>
-          <button><img src="/assets/media/dash/send.png" alt=""/></button>
-        </div>
-        
-        
-        
-        <div className="post_comments">
-          <div className="pop_comment">Popular Comments</div>
-          <div className="comments_point">
-            <div className="fire"><img src="/assets/media/dash/fire.png" alt=""/> <span>45</span></div>
-            <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
-            <h3>TheMadTitan</h3>
-            <a href="#" className="create">Creator</a> <span className="days">2 days ago</span> <a href="#" className="pinned">Pinned by Creator</a> </div>
-          <p>Thank you everyone for all of your support.</p>
-          <div className="loadmore"><a href="#">Load comments <i className="fa fa-angle-down"
-                                    aria-hidden="true"></i></a></div>
-        </div>
-        
-        
       </div>
-    </div>
-    
-    <div className="post">
-      <div className="heads">
-        <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
-        <h4>TheMadTitan</h4>
-      </div>
-      <div className="left_details"> <a href="#"> <i className="fa fa-heart" aria-hidden="true"></i> <span>1.7k</span> </a> <a href="#"> <i className="fa fa-eye" aria-hidden="true"></i> <span>239k</span> </a> <a href="#"> <i className="fa fa-commenting" aria-hidden="true"></i> <span>232k</span> </a> </div>
-      <div className="right_details">
-        <div className="post_data"></div>
-        <div className="users_share_box">
-          <div className="more_user"> <a href="#"><img src="/assets/media/dash/1.jpg" alt="user"/><span className="online"></span></a> <a href="#"><img src="/assets/media/dash/2.jpg" alt="user"/><span className="online"></span></a> <a href="#"><img src="/assets/media/dash/3.jpg" alt="user"/><span className="offiline"></span></a> <a href="#" className="more">+3</a> <span className="others">Ashwin, George and 5 others have liked your post.</span> </div>
-          <div className="shere"> <a href="#"> <i className="fa fa-heart" aria-hidden="true"></i> <span>Like</span> </a> <a href="#"> <i className="fa fa-share-alt" aria-hidden="true"></i> <span>Share</span> </a>
-            <div className="three_dots"><a href="#"> <i className="fa fa-ellipsis-v" aria-hidden="true"></i></a>
-              <div className="three_dots_dropdown">
-                <ul>
-                  <li><a href="#">Edit</a></li>
-                  <li><a href="#">Share to</a></li>
-                  <li><a href="#">Copy Link</a></li>
-                  <li><a href="#">Delet</a></li>
-                </ul>
+      
+      <div className="post">
+        <div className="heads">
+          <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
+          <h4>TheMadTitan</h4>
+        </div>
+        <div className="left_details"> <a href="#"> <i className="fa fa-heart" aria-hidden="true"></i> <span>1.7k</span> </a> <a href="#"> <i className="fa fa-eye" aria-hidden="true"></i> <span>239k</span> </a> <a href="#"> <i className="fa fa-commenting" aria-hidden="true"></i> <span>232k</span> </a> </div>
+        <div className="right_details">
+          <div className="post_data"></div>
+          <div className="users_share_box">
+            <div className="more_user"> <a href="#"><img src="/assets/media/dash/1.jpg" alt="user"/><span className="online"></span></a> <a href="#"><img src="/assets/media/dash/2.jpg" alt="user"/><span className="online"></span></a> <a href="#"><img src="/assets/media/dash/3.jpg" alt="user"/><span className="offiline"></span></a> <a href="#" className="more">+3</a> <span className="others">Ashwin, George and 5 others have liked your post.</span> </div>
+            <div className="shere"> <a href="#"> <i className="fa fa-heart" aria-hidden="true"></i> <span>Like</span> </a> <a href="#"> <i className="fa fa-share-alt" aria-hidden="true"></i> <span>Share</span> </a>
+              <div className="three_dots"><a href="#"> <i className="fa fa-ellipsis-v" aria-hidden="true"></i></a>
+                <div className="three_dots_dropdown">
+                  <ul>
+                    <li><a href="#">Edit</a></li>
+                    <li><a href="#">Share to</a></li>
+                    <li><a href="#">Copy Link</a></li>
+                    <li><a href="#">Delet</a></li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
+          
+          
+          <div className="add_comment_box">
+            <div className="add_comments">
+              <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
+              <textarea placeholder="Add a comment"></textarea>
+              <a href="#" className="gif">GIF</a> <a href="#" className="smile"><img src="/assets/media/dash/smile.png" alt=""/></a> </div>
+            <button><img src="/assets/media/dash/send.png" alt=""/></button>
+          </div>
+          
+          
         </div>
-        
-        
-        <div className="add_comment_box">
-          <div className="add_comments">
-            <div className="user"><img src="/assets/media/dash/user.jpg" alt=""/></div>
-            <textarea placeholder="Add a comment"></textarea>
-            <a href="#" className="gif">GIF</a> <a href="#" className="smile"><img src="/assets/media/dash/smile.png" alt=""/></a> </div>
-          <button><img src="/assets/media/dash/send.png" alt=""/></button>
-        </div>
-        
-        
       </div>
+      
     </div>
+    
     
   </div>
   
   
-</div>
-
-
-
-    );
-
-    export default SignedMainContent;
+  
+      );
+  
+      export default SignedMainContent;
