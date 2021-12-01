f672dd1b1198ac669866e42d8ed0d251d504d27a
ajay <ajay.thakur@anthive.tech>
2021-12-01 11:54:04 +0530

    css

--- components/calendar/date.js
+++ components/calendar/date.js
@@ -1,26 +1,99 @@
+
 import React from "react";
-import TimeCalendar from "react-timecalendar";
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import baseURL from '../../utils/baseURL';
+import { QueryClient, QueryClientProvider, useMutation } from 'react-query'
 
-const openHours = [
-  [9.5, 15],
-  [9, 23.5],
-];
-function loggingTime(time) {
-  console.log(time);
-}
-const Date = () => (
 
-  <div className="calendar_box">  
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format } from 'date-fns'
+
+
+const DateCal = () =>{
+
+
+  const [startDates, setStartDate] = useState(new Date("2021/11/01"));
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
+  console.log(startDates)
   
-  <TimeCalendar
-    clickable
-    timeSlot={30}
-    openHours={false}
 
-  />
+  const [tournament, SetTournament] = useState([])
+  
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/api/tournaments/`)
+      .then((res) => {
+        SetTournament(res.data);
+       // console.log(tournament)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
+ //const result = tournament.filter(d=>(d.tournament.region) == "BHARAT");
+
+ const result = tournament.filter(d=>(d.tournament.startDate) === startDates)
+
+  
+
+  console.log(result)
+
+
+return (
+
+
+
+  <>
+  <div className="calendar_box">  
+
+  <DatePicker
+      selected={startDates}
+      onChange={onChange}
+      startDates={startDates}
+      endDate={endDate}
+      monthsShown={1}
+      selectsRange
+      inline
+      dateFormat="MMMM d, yyyy"
+      
+      
+    />
+
+<div>Date - {}</div>
 
 </div>
 
-);
+<div>
 
-export default Date;
+{result.length === 0 ? (<p>No data</p>) : (<div>{result.map((tour)=>(
+  <div key={tour._id}>
+  <p>{tour.tournament.name}</p> 
+  <p>{tour.tournament.description}</p> 
+  <p>{tour.tournament.status}</p> 
+  <p>{tour.tournament.tournament_type}</p> 
+  <p>{tour.tournament.prizepool}</p> 
+  
+  </div>))}</div>
+  )}
+
+
+
+
+</div>
+
+
+</>
+
+)
+};
+export default DateCal;
--- components/calendar/match.js
+++ components/calendar/match.js
@@ -1,109 +1,46 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import baseURL from '../../utils/baseURL';
+import { QueryClient, QueryClientProvider, useMutation } from 'react-query'
 
 
+const Match = () => {
 
-const Match = () => (
+  const [tournament, SetTournament] = useState([])
 
+  useEffect(() => {
+    axios
+      .get(`${baseURL}/api/tournaments/`)
+      .then((res) => {
+        SetTournament(res.data);
+        console.log(tournament)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+ 
     
-    <div className="white_bg">
+return(
 
-    <div className="all_matches">
-    <h2>Xenowatch league-week6</h2>
-    <div className="match_box">
-      
+  <>
+  <h1>Match</h1>
 
-      <div className="match_table">
-        <div className="head_row">
-          <div className="tm">11:00PCT</div>
-          <div className="live"><b>Live</b> 25:20</div>
-        </div>
-        <div className="data_col1">
-          <div className="top">
-            <div className="lft_dp"> <span className="dp"><img src="/assets/media/calendar/first.jpg" alt="" /></span> <span className="dp_name">FayeDBebop<b>Sarah Valentine</b></span> </div>
-            <div className="num">3</div>
-          </div>
-          <div className="top">
-            <div className="lft_dp"> <span className="dp"><img src="/assets/media/calendar/first.jpg" alt="" /></span> <span className="dp_name">FayeDBebop<b>Sarah Valentine</b></span> </div>
-            <div className="num">3</div>
-          </div>
-        </div>
-        <div className="data_col2"> <a href="#"><img src="/assets/media/calendar/stats.jpg" /> Full Match Stats</a> <a href="#"><img src="/assets/media/calendar/play.jpg" /> Watch Replay</a> </div>
-      </div>
 
-      <div className="match_table">
-        <div className="head_row">
-          <div className="tm">11:00PCT</div>
-          <div className="live"><b>Live</b> 25:20</div>
-        </div>
-        <div className="data_col1">
-          <div className="top">
-            <div className="lft_dp"> <span className="dp"><img src="/assets/media/calendar/first.jpg" alt="" /></span> <span className="dp_name">FayeDBebop<b>Sarah Valentine</b></span> </div>
-            <div className="num">3</div>
-          </div>
-          <div className="top">
-            <div className="lft_dp"> <span className="dp"><img src="/assets/media/calendar/first.jpg" alt="" /></span> <span className="dp_name">FayeDBebop<b>Sarah Valentine</b></span> </div>
-            <div className="num">3</div>
-          </div>
-        </div>
-        <div className="data_col2"> <a href="#"><img src="/assets/media/calendar/stats.jpg" /> Full Match Stats</a> <a href="#"><img src="images/calendar/play.jpg" /> Watch Replay</a> </div>
-      </div>
+  {tournament.length === 0 ? (<p>No data</p>) : (<div>{tournament.map((tour)=>(
+  <div key={tour._id}>
+  <p>{tour.tournament.name}</p> </div>))}</div>
+ 
+  
+  )}
 
 
 
-      <div className="match_table">
-        <div className="head_row">
-          <div className="tm">11:00PCT</div>
-          <div className="live"><b>Live</b> 25:20</div>
-        </div>
-        <div className="data_col1">
-          <div className="top">
-            <div className="lft_dp"> <span className="dp"><img src="/assets/media/calendar/first.jpg" alt="" /></span> <span className="dp_name">FayeDBebop<b>Sarah Valentine</b></span> </div>
-            <div className="num">3</div>
-          </div>
-          <div className="top">
-            <div className="lft_dp"> <span className="dp"><img src="/assets/media/calendar/first.jpg" alt="" /></span> <span className="dp_name">FayeDBebop<b>Sarah Valentine</b></span> </div>
-            <div className="num">3</div>
-          </div>
-        </div>
-        <div className="data_col2"> <a href="#"><img src="/assets/media/calendar/stats.jpg" /> Full Match Stats</a> <a href="#"><img src="images/calendar/play.jpg" /> Watch Replay</a> </div>
-      </div>
+  </>
 
 
+)
+}
 
-    </div>
-  </div>
-
-  <div className="all_matches">
-    <h2>League of Heroes Touranament Qualifiers</h2>
-    <div className="match_box">
-      
-
-      <div className="match_table">
-        <div className="head_row">
-          <div className="tm">11:00PCT</div>
-          <div className="live"><b>Live</b> 25:20</div>
-        </div>
-        <div className="data_col1">
-          <div className="top">
-            <div className="lft_dp"> <span className="dp"><img src="/assets/media/calendar/first.jpg" alt="" /></span> <span className="dp_name">FayeDBebop<b>Sarah Valentine</b></span> </div>
-            <div className="num">3</div>
-          </div>
-          <div className="top">
-            <div className="lft_dp"> <span className="dp"><img src="/assets/media/calendar/first.jpg" alt="" /></span> <span className="dp_name">FayeDBebop<b>Sarah Valentine</b></span> </div>
-            <div className="num">3</div>
-          </div>
-        </div>
-        <div className="data_col2"> <a href="#"><img src="/assets/media/calendar/stats.jpg" /> Full Match Stats</a> <a href="#"><img src="/assets/media/calendar/play.jpg" /> Watch Replay</a> </div>
-      </div>
-
-
-
-    </div>
-  </div>
-
-
-  </div>
-
-
-);
-
-export default Match;               
+export default Match;
