d7c859e724c4d9203a32d8500ec14cdbe9b7e580
ajay <ajay.thakur@anthive.tech>
2021-12-06 17:53:06 +0530

    all script

--- components/calendar/date.js
+++ components/calendar/date.js
@@ -1,20 +1,14 @@
-
-import React from "react";
-import { useState, useEffect } from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import baseURL from '../../utils/baseURL';
-import { QueryClient, QueryClientProvider, useMutation } from 'react-query'
+import { QueryClient, QueryClientProvider, useMutation } from 'react-query';
 
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { format } from 'date-fns'
-
-
-const DateCal = () =>{
-
-
-  const [startDates, setStartDate] = useState(new Date("2021/11/01"));
+const DateCal = () => {
+  const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(null);
   const onChange = (dates) => {
     const [start, end] = dates;
@@ -22,78 +16,79 @@ const DateCal = () =>{
     setEndDate(end);
   };
 
-  console.log(startDates)
-  
+  function formatDate(date) {
+    var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
 
-  const [tournament, SetTournament] = useState([])
-  
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
+
+  //console.log(formatDate(startDate));
+  //console.log(formatDate(endDate));
+
+  const [tournament, SetTournament] = useState([]);
+
   useEffect(() => {
     axios
       .get(`${baseURL}/api/tournaments/`)
       .then((res) => {
         SetTournament(res.data);
-       // console.log(tournament)
+        // console.log(tournament)
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+  //const result = tournament.filter(d=>(d.tournament.region) == "BHARAT");
 
- //const result = tournament.filter(d=>(d.tournament.region) == "BHARAT");
+  var result = tournament.filter((a) => {
+    var dates = formatDate(a.tournament.startDate);
+    return dates >= formatDate(startDate) && dates <= formatDate(endDate);
+  });
 
- const result = tournament.filter(d=>(d.tournament.startDate) === startDates)
+  console.log(result);
 
-  
+  return (
+    <>
+      <div className="calendar_box">
+        <DatePicker
+          selected={startDate}
+          onChange={onChange}
+          startDate={startDate}
+          endDate={endDate}
+          monthsShown={1}
+          selectsRange
+          inline
+          dateFormat="MMMM d, yyyy"
+        />
 
-  console.log(result)
+        <div>Date - {}</div>
+      </div>
 
-
-return (
-
-
-
-  <>
-  <div className="calendar_box">  
-
-  <DatePicker
-      selected={startDates}
-      onChange={onChange}
-      startDates={startDates}
-      endDate={endDate}
-      monthsShown={1}
-      selectsRange
-      inline
-      dateFormat="MMMM d, yyyy"
-      
-      
-    />
-
-<div>Date - {}</div>
-
-</div>
-
-<div>
-
-{result.length === 0 ? (<p>No data</p>) : (<div>{result.map((tour)=>(
-  <div key={tour._id}>
-  <p>{tour.tournament.name}</p> 
-  <p>{tour.tournament.description}</p> 
-  <p>{tour.tournament.status}</p> 
-  <p>{tour.tournament.tournament_type}</p> 
-  <p>{tour.tournament.prizepool}</p> 
-  
-  </div>))}</div>
-  )}
-
-
-
-
-</div>
-
-
-</>
-
-)
+      <div>
+        {result.length === 0 ? (
+          <p>No data</p>
+        ) : (
+          <div>
+            {result.map((tour) => (
+              <div key={tour._id}>
+                <p>{tour.tournament.name}</p>
+                <p>{tour.tournament.description}</p>
+                <p>{tour.tournament.status}</p>
+                <p>{tour.tournament.tournament_type}</p>
+                <p>{tour.tournament.prizepool}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 export default DateCal;
