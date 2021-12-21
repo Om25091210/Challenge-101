053218a65ac94b26898ca99135a27ecc2fce2379
ajay <ajay.thakur@anthive.tech>
2021-12-21 17:46:56 +0530

    css

--- components/LeftNav.js
+++ components/LeftNav.js
@@ -1,32 +1,87 @@
-import DarkMode from './theme/dark-mode'
+import DarkMode from './theme/dark-mode';
 import Link from 'next/link';
 
-
 function LeftNav() {
-
   return (
+    <div className="left_side">
+      <nav>
+        <ul>
+          <li>
+            <Link href="/dashboard">
+              <a className="active">
+                <span className="iconbg">
+                  <i className="fa fa-home" aria-hidden="true"></i>
+                </span>{' '}
+                <span className="title">HOME</span>
+              </a>
+            </Link>{' '}
+          </li>
+          <li>
+            <Link href="/ranking">
+              <a className="">
+                <span className="iconbg">
+                  <i className="fa fa-trophy" aria-hidden="true"></i>
+                </span>{' '}
+                <span className="title">RANKING</span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/tournament">
+              <a className="">
+                <span className="iconbg">
+                  <i className="fa fa-flag" aria-hidden="true"></i>
+                </span>{' '}
+                <span className="title">TOURNAMENTS</span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/games">
+              <a className="">
+                <span className="iconbg">
+                  <i className="fa fa-gamepad" aria-hidden="true"></i>
+                </span>{' '}
+                <span className="title">GAMES</span>
+              </a>
+            </Link>{' '}
+          </li>
+          <li>
+            <Link href="/discover">
+              <a className="">
+                <span className="iconbg">
+                  <i className="fa fa-compass" aria-hidden="true"></i>
+                </span>{' '}
+                <span className="title">DISCOVER</span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/calendar">
+              <a className="">
+                <span className="iconbg">
+                  <i className="fa fa-calendar" aria-hidden="true"></i>
+                </span>{' '}
+                <span className="title">CALENDER</span>
+              </a>
+            </Link>{' '}
+          </li>
+          <li>
+            <Link href="/">
+              <a className="">
+                <span className="iconbg">
+                  <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                </span>{' '}
+                <span className="title">SHOP</span>
+              </a>
+            </Link>{' '}
+          </li>
+        </ul>
+      </nav>
 
-
-<div className="left_side">
-  <nav>
-    <ul>
-      <li><Link href="/dashboard"><a  className="active"><span className="iconbg"><i className="fa fa-home" aria-hidden="true"></i></span> <span className="title">HOME</span></a></Link> </li>
-      <li><Link href="/ranking"><a  className=""><span className="iconbg"><i className="fa fa-trophy" aria-hidden="true"></i></span> <span className="title">RANKING</span></a></Link></li>
-      <li><Link href="/tournament"><a  className=""><span className="iconbg"><i className="fa fa-flag" aria-hidden="true"></i></span> <span className="title">TOURNAMENTS</span></a></Link></li>
-      <li><Link  href="/games"><a className=""><span className="iconbg"><i className="fa fa-gamepad" aria-hidden="true"></i></span> <span className="title">GAMES</span></a></Link> </li>
-      <li><Link  href="/discover"><a className=""><span className="iconbg"><i className="fa fa-compass" aria-hidden="true"></i></span> <span className="title">DISCOVER</span></a></Link></li>
-      <li><Link href="/calendar"><a  className=""><span className="iconbg"><i className="fa fa-calendar" aria-hidden="true"></i></span> <span className="title">CALENDER</span></a></Link> </li>
-    </ul>
-  </nav>
-
-
- <DarkMode />
-
-</div>
-
-  )
+      <DarkMode />
+    </div>
+  );
 }
 
-
-export default LeftNav
-
+export default LeftNav;
