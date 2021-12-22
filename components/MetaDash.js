63323c3664b804e1ac17fca0dbe7c5e97983a15a
ajay <ajay.thakur@anthive.tech>
2021-12-22 15:28:25 +0530

    js issue

--- components/MetaDash.js
+++ components/MetaDash.js
@@ -60,15 +60,6 @@ const MetaDash = () => (
       rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"
     />
-
-    <script src="/assets/plugins/global/plugins.bundle.js" />
-    <script src="/assets/js/scripts.bundle.js" />
-    <script src="/assets/plugins/global/plugins.bundle.js" />
-    <script src="/assets/js/dash/bootstrap.bundle.min.js" />
-    <script src="/assets/js/dash/jquery.mCustomScrollbar.js" />
-    <script src="/assets/js/dash/jquery.fancybox.js" />
-    <script src="/assets/js/dash/slick.js" />
-    <script src="/assets/js/dash/custom.js" />
   </Head>
 );
 
--- pages/AllScript.js
+++ pages/AllScript.js
@@ -1,7 +1,21 @@
 import Script from 'next/script';
 
+import Customjs from './newcustom';
+
 const AllScript = () => {
-  return <></>;
+  return (
+    <>
+      <script src="/assets/plugins/global/plugins.bundle.js" />
+      <script src="/assets/js/scripts.bundle.js" />
+      <script src="/assets/plugins/global/plugins.bundle.js" />
+      <script src="/assets/js/dash/bootstrap.bundle.min.js" />
+      <script src="/assets/js/dash/jquery.mCustomScrollbar.js" />
+      <script src="/assets/js/dash/jquery.fancybox.js" />
+      <script src="/assets/js/dash/slick.js" />
+      {/* <script src="/assets/js/dash/custom.js" /> */}
+      <Customjs />
+    </>
+  );
 };
 
 export default AllScript;
