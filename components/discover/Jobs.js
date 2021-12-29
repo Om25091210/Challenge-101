0f8ed0528db82a740f784a2888a920fdf56c5df6
Shan-RL <>
2021-12-29 21:55:18 +0530

    Jobs: RL

--- components/discover/Jobs.js
+++ components/discover/Jobs.js
@@ -4,13 +4,43 @@ import axios from 'axios';
 import baseURL from '@utils/baseURL';
 import Filters from '../common/Filters';
 import FileDropzone from '@components/common/FileDropzone';
+import cookie from 'js-cookie';
+import { useMutation } from 'react-query';
+import { toast } from 'react-toastify';
 
 const Jobs = () => {
   const [data, setData] = useState(null);
   const [jobs, setJobs] = useState(null);
-  const [files, setFiles] = useState();
+  const [files, setFiles] = useState([]);
 
   var ftype = 'JOBS';
+
+  const mutation = useMutation(
+    async (formdata) =>
+      await axios.post(`${baseURL}/api/uploads/uploadfile`, formdata, {
+        headers: {
+          Authorization: cookie.get('token'),
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+  );
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formdata = new FormData();
+    for (const key of Object.keys(files)) {
+      formdata.append('file', files[key]);
+    }
+    try {
+      await mutation.mutateAsync(formdata);
+      toast.success('Your file has been successfully uploaded');
+    } catch (err) {
+      toast.error(err.response?.data?.msg || 'Please recheck your upload');
+    }
+  };
+
+
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(`${baseURL}/api/filters/${ftype}`);
@@ -101,7 +131,7 @@ const Jobs = () => {
             setFiles={setFiles}
           />
 
-            {files.map((file, idx) => ( <> {file.name}  :  <a href="#" className="logo_box">APPLY NOW  </a></> ))}
+            {files.map((file, idx) => ( <> {file.name}  :  <a href="#" onClick={onSubmit} className="logo_box">APPLY NOW  </a></> ))}
 
 
 
