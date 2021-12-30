d03da868fbf2f0da2f0a77a9f4b909eb5a7cc644
ajay <ajay.thakur@anthive.tech>
2021-12-30 08:51:53 +0530

    css

--- pages/AllScript.js
+++ pages/AllScript.js
@@ -5,13 +5,13 @@ import Customjs from './newcustom';
 const AllScript = () => {
   return (
     <>
-      <script src="/assets/plugins/global/plugins.bundle.js" />
-      <script src="/assets/js/scripts.bundle.js" />
-      <script src="/assets/plugins/global/plugins.bundle.js" />
-      <script src="/assets/js/dash/bootstrap.bundle.min.js" />
-      <script src="/assets/js/dash/jquery.mCustomScrollbar.js" />
-      <script src="/assets/js/dash/jquery.fancybox.js" />
-      <script src="/assets/js/dash/slick.js" />
+      <script async src="/assets/plugins/global/plugins.bundle.js" />
+      <script async src="/assets/js/scripts.bundle.js" />
+      <script async src="/assets/plugins/global/plugins.bundle.js" />
+      <script async src="/assets/js/dash/bootstrap.bundle.min.js" />
+      <script async src="/assets/js/dash/jquery.mCustomScrollbar.js" />
+      <script async src="/assets/js/dash/jquery.fancybox.js" />
+      <script async src="/assets/js/dash/slick.js" />
       {/* <script src="/assets/js/dash/custom.js" /> */}
       <Customjs />
     </>
