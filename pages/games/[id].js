4d86d938bcc8fac49c214603cd17e25032fd1558
Shan-RL <>
2022-01-05 08:45:22 +0530

    Games name issue:RL

--- pages/games/[id].js
+++ pages/games/[id].js
@@ -2,7 +2,7 @@ import MetaDash from '@components/MetaDash';
 import SignedHeader from '@components/SignedHeader';
 import LeftNav from '@components/LeftNav';
 import AllScript from '../AllScript';
-import Game from '@components/games';
+import Game from '@components/Game';
 
 const Games = ({ user }) => {
   return (
