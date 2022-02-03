bc0281f1491c81c50928b9ecf76855eae6773d4a
ajay <ajay.thakur@anthive.tech>
2022-02-03 16:21:28 +0530

    css

--- components/profile/ProfileMatches.js
+++ components/profile/ProfileMatches.js
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react';
 import { useMutation } from 'react-query';
 import { toast } from 'react-toastify';
@@ -8,37 +7,41 @@ import axios from 'axios';
 import baseURL from '@utils/baseURL';
 import Moment from 'moment';
 
-
 const ProfileMatches = ({ user, Userdata }) => {
-
   return (
-
-        <div className="tab hide" id="matches">
+    <div className="tab hide" id="matches">
+      {' '}
+      {Userdata.teamMatchesList.length == 0 ? (
+        <p>
           {' '}
+          Player info is not attached to the profile. Please ensure that player
+          is assigned to this profile.{' '}
+        </p>
+      ) : (
+        Userdata.teamMatchesList.map((result, index) => (
+          <div className="next_matches" key={index}>
+            <div className="bdr_clr_green">
+              <div className="single_team">
+                <h2>Team : </h2>
+                <h3>{result.team.name} </h3>
+                <img src={result.team.imgUrl} alt="" />
+              </div>
 
-
-{ Userdata.teamMatchesList.length == 0 ? ( <p> Player info is not attached to the profile. Please ensure that player is assigned to this profile. </p>) :
-
-
-
-
-
-
-
-  Userdata.teamMatchesList.map((result, index) => (
-            <div className="next_matches" key={index}>
-
-            <div className="bdr_clr_green">{result.team.name} {' '} 
-              <img src={result.team.imgUrl} style={{width:'10%'}} alt="" />
-
-             <b>PLAYERS :</b> 
-            {result.team.players.map((player, index) => (
-           <> <span>{player.playerId.name}</span>    <img src={player.playerId.imgUrl} style={{width: '5%'}} alt="" /></>
-                
-            )) }
-
+              <div className="user_vs_player">
+                <h2>PLAYERS :</h2>
+                <ul>
+                  {result.team.players.map((player, index) => (
+                    <>
+                      {' '}
+                      <li>
+                        <img src={player.playerId.imgUrl} alt="" />{' '}
+                        <h4>{player.playerId.name}</h4>
+                      </li>
+                    </>
+                  ))}
+                </ul>
+              </div>
             </div>
-
 
             <div className="stats_table">
               <table className="table">
@@ -63,27 +66,26 @@ const ProfileMatches = ({ user, Userdata }) => {
                             {Moment(match.startDate).format('DD-MM-YYYY')}
                           </td>
                           <td>
-
-                            { match.opponents.map((res, idx) => (
-                             result.team._id != res.opponent._id ? (
-                            <>  
-                            <span className="dp">
-                              <img src={res.opponent.image_url} alt="" />
-                            </span>{' '}
-                            <span className="dp_name">
-                              <b>{res.opponent.name}</b> 
-                            </span>
-                            </>
-
-                              ) : ''
-
-                              ))}
+                            {match.opponents.map((res, idx) =>
+                              result.team._id != res.opponent._id ? (
+                                <>
+                                  <span className="dp">
+                                    <img src={res.opponent.image_url} alt="" />
+                                  </span>{' '}
+                                  <span className="dp_name">
+                                    <b>{res.opponent.name}</b>
+                                  </span>
+                                </>
+                              ) : (
+                                ''
+                              )
+                            )}
                           </td>
                           <td>{match.name}</td>
                           <td>Semi-Finals</td>
                           <td>{Moment(match.startDate).format('h:m')}</td>
                           <td>
-                  			{match.results[0].score} - {match.results[1].score}
+                            {match.results[0].score} - {match.results[1].score}
                           </td>
                           <td>View Match</td>
                           <td>
@@ -96,13 +98,10 @@ const ProfileMatches = ({ user, Userdata }) => {
                 </tbody>
               </table>
             </div>
-
           </div>
-
-  ))}
-
-       </div> 
-
+        ))
+      )}
+    </div>
   );
 };
 
