9d90a7d8572d2096edfeebfbaed06ed3db148656
ajay <ajay.thakur@anthive.tech>
2022-02-03 15:12:39 +0530

    css

--- components/product/ProductItem.js
+++ components/product/ProductItem.js
@@ -22,9 +22,9 @@ const ProductItem = ({ product, handleCheck }) => {
       <div className="pro_bottom">
         <div className="name_dots">
           <h4> {product.title} </h4>
-          <a href="#">
+          {/* <a href="#">
             <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
-          </a>{' '}
+          </a>{' '} */}
         </div>
         <div className="stars">
           {' '}
@@ -43,26 +43,28 @@ const ProductItem = ({ product, handleCheck }) => {
           <a href="#">
             <i className="fa fa-star" aria-hidden="true"></i>
           </a>
-          <div className="price"> ${product.price}</div>
         </div>
-      </div>
-      <div className="likes">
-        <a href="#">
-          <i className="fa fa-heart" aria-hidden="true"></i> 80{' '}
-        </a>
-      </div>
 
-      <button
-        className="cart-btn btn btn-info"
-        disabled={product.inStock === 0 ? true : false}
-        onClick={() => {
-          dispatch(addToCart(product, cart));
-          setClicked(true);
-        }}
-      >
-        {clicked ? 'Item in Cart' : 'Add to Cart'}
-        <i className="fa fa-cart-plus pl-2"></i>
-      </button>
+        <div className="likes_price">
+          <div className="price"> ${product.price}</div>
+
+          <a href="#">
+            <i className="fa fa-heart" aria-hidden="true"></i> 80{' '}
+          </a>
+        </div>
+
+        <button
+          className="cart-btn btn btn-info"
+          disabled={product.inStock === 0 ? true : false}
+          onClick={() => {
+            dispatch(addToCart(product, cart));
+            setClicked(true);
+          }}
+        >
+          {clicked ? 'Item in Cart' : 'Add to Cart'}
+          <i className="fa fa-cart-plus pl-2"></i>
+        </button>
+      </div>
     </>
   );
 };
