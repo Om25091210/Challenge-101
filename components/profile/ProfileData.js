a39802b74d244f68fa056a5ddf16bf1c17c7bd1a
Amogh-Ak <59502561+Amogh-Ak@users.noreply.github.com>
2022-02-16 13:59:24 +0530

    Profile:Notification

--- components/profile/ProfileData.js
+++ components/profile/ProfileData.js
@@ -1,14 +1,5 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-import baseURL from '@utils/baseURL';
-import CustomPost from '../dashboard/CustomPost';
-import LikePost from '../postLikes/LikePost';
-import CommentForm from '../comments/CommentForm';
 import ProdPoup from '../profile/prodPoup';
-import Moment from 'moment';
-import { useMutation } from 'react-query';
-import { toast } from 'react-toastify';
-import cookie from 'js-cookie';
 import Photos from './Photos';
 import Videos from './Videos';
 import ProductList from '@components/common/ProductList';
@@ -16,7 +7,7 @@ import ProductRigs from '@components/common/ProductRigs';
 import ProfileMatches from './ProfileMatches';
 import TeamAllStats from '@components/team/TeamAllStats';
 import GamesDetails from './GamesDetails';
-
+import AllPosts from '@components/dashboard/AllPosts';
 
 const ProfileData = ({ user, Userdata, player, products }) => {
   const [profile, setProfile] = useState(Userdata.profile);
@@ -33,134 +24,26 @@ const ProfileData = ({ user, Userdata, player, products }) => {
           <div className="profile_left_post">
             <div className="">
               {Userdata.posts.length !== 0 &&
-                Userdata.posts.map((post, index) => (
-                  <div className="post">
-                    <div key={index}>
-                      <div className="heads" key={index}>
-                        <div className="user">
-                          <img src={post.user.profilePicUrl} alt="" />
-                        </div>
-                        <div className="user_name_disc">
-                          <h4>{post.user.username}</h4>
-                          <p>{post.description}</p>
-                        </div>
-
-                        <div className="date">
-                          {post.createdAt === post.updatedAt ? (
-                            <p>
-                              {' '}
-                              {Moment(post.createdAt).format(
-                                'MMMM, DD, YYYY hh:mm A'
-                              )}{' '}
-                            </p>
-                          ) : (
-                            <p>
-                              {' '}
-                              {Moment(post.updatedAt).format(
-                                'MMMM, DD, YYYY hh:mm A'
-                              )}{' '}
-                            </p>
-                          )}
-                        </div>
-                      </div>
-                      <div className="left_details">
-                        {' '}
-                        <a href="#">
-                          {' '}
-                          <i
-                            className="fa fa-heart"
-                            aria-hidden="true"
-                          ></i>{' '}
-                          <span>{post.likes.length}</span>{' '}
-                        </a>{' '}
-                        <a href="#">
-                          {' '}
-                          <i className="fa fa-eye" aria-hidden="true"></i>{' '}
-                          <span>{post.views}</span>{' '}
-                        </a>{' '}
-                        <a href="#">
-                          {' '}
-                          <i
-                            className="fa fa-commenting"
-                            aria-hidden="true"
-                          ></i>{' '}
-                          <span>0</span>{' '}
-                        </a>{' '}
-                      </div>
-                      <div className="right_details">
-                        <div className="post_data">
-                          <img src={post.images} alt="" />
-                        </div>
-                        <div className="users_share_box">
-                          <div className="more_user">
-                            {' '}
-                            <a href="#">
-                              <img src="/assets/media/dash/1.jpg" alt="user" />
-                              <span className="online"></span>
-                            </a>{' '}
-                            <a href="#">
-                              <img src="/assets/media/dash/2.jpg" alt="user" />
-                              <span className="online"></span>
-                            </a>{' '}
-                            <a href="#">
-                              <img src="/assets/media/dash/3.jpg" alt="user" />
-                              <span className="offiline"></span>
-                            </a>{' '}
-                            <a href="#" className="more">
-                              +3
-                            </a>{' '}
-                            <span className="others">
-                              Ashwin, George and 5 others have liked your post.
-                            </span>{' '}
-                          </div>
-                          <div className="shere">
-                            {' '}
-                            <LikePost postId={post._id} />{' '}
-                            <a href="#">
-                              {' '}
-                              <i
-                                className="fa fa-share-alt"
-                                aria-hidden="true"
-                              ></i>{' '}
-                              <span>Share</span>{' '}
-                            </a>
-                            <div className="three_dots">
-                              <a>
-                                {' '}
-                                <i
-                                  className="fa fa-ellipsis-v"
-                                  aria-hidden="true"
-                                ></i>
-                              </a>
-                              <CustomPost post={post} />
-                            </div>
-                          </div>
-                        </div>
-
-                        <CommentForm post={post} user={user} />
-                      </div>
-                    </div>
-                  </div>
+                Userdata.posts.map((post) => (
+                  <AllPosts post={post} user={user} />
                 ))}
             </div>
           </div>
 
           <div className="profile_match_details">
-
             {Userdata.teamMatchesList.map((result, index) => (
-                  <TeamAllStats teamId={result.team._id} />
-              ))}
+              <TeamAllStats teamId={result.team._id} />
+            ))}
 
             <GamesDetails />
-
           </div>
         </div>
 
-{/* 
+        {/* 
         <div className="tab hide" id="statistics">
 
         </div>
-*/}        
+*/}
         <div className="tab hide" id="achievement">
           {' '}
           <div className="achivement_box">
@@ -246,8 +129,7 @@ const ProfileData = ({ user, Userdata, player, products }) => {
 
         <ProfileMatches user={user} Userdata={Userdata} />
 
-        <ProductList user={user} productList={products}/>
-
+        <ProductList user={user} productList={products} />
 
         <div className="tab hide" id="photos">
           <Photos Userdata={Userdata} />
@@ -282,8 +164,7 @@ const ProfileData = ({ user, Userdata, player, products }) => {
           </div>
         </div>
 
-        <ProductRigs user={user} productList={products}/>
-
+        <ProductRigs user={user} productList={products} />
       </div>
 
       {/* ------------- start poup data ------------- */}
