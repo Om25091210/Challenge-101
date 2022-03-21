6953d78ffd5e11feb4785c4dcd4326051e0dece4
Shan-RL <>
2022-03-21 22:09:21 +0530

    Fixe:

--- components/common/Loader.tsx
+++ components/common/Loader.tsx
@@ -1,22 +1,20 @@
 import React from "react";
-import Lottie from "lottie-react";
+
+import { useLottie } from "lottie-react";
 import * as loaderData2 from "../../animations/loading2.json";
 
 const defaultOptions = {
   loop: true,
   autoplay: true,
   animationData: loaderData2,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
 };
 
 interface Props {}
 
 export const Loader = (props: Props) => {
-  return (
-    <div>
-      <Lottie options={defaultOptions} />
-    </div>
-  );
+
+  const { View } = useLottie(options, style);
+
+  return View;
+  
 };
