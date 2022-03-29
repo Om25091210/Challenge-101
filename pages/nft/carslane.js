038b029c00d307ec5052689609f2560ba0d42d6f
Shan-RL <>
2022-03-29 18:57:23 +0530

    nftgames:cars

--- pages/nft/carslane.js
+++ pages/nft/carslane.js
@@ -18,7 +18,7 @@ const Carslane = ({ user }) => {
 <iframe id="carslaneid"
     title="carslane Frame"
 style={{border : 'none', margin:'0', padding:'0', position:'absolute', top:'0px', left:'0px', bottom:'0px',right:'0px',  width: '100%' , height: '100%', frameborder:'0',  seamless:'seamless', scrolling:'no'}}
-     src='http://127.0.0.1:8080/index.html' width="100%" height="100%">
+     src={{url}} width="100%" height="100%">
 </iframe>
 
 
