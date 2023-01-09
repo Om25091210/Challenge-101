7ad695234408917110980b164fb81aec903bb66d
Omkar <omkartikare55@gmail.com>
2023-01-09 13:16:26 +0530

    ClashRoyal:API

--- components/SignedHeader.js
+++ components/SignedHeader.js
@@ -291,7 +291,7 @@ const SignedHeader = ({ user, profile }) => {
                   <Link href="/dashboard">Dashboard</Link>
                 </li>
                 <li>
-                  <Link href="/settings/general">Settings and General</Link>
+                  <Link href="/settings/general">Settings</Link>
                 </li>
                 <li>
                   <Link href="/myNFTs">My NFTs</Link>
